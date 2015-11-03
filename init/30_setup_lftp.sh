84a2fa7e9b3f819c87788670936b639e2662e8b3
Rob Shad <robertmshad@googlemail.com>
2015-11-02 22:29:15 -0500

    adds icon and fixes cron job

--- init/30_setup_lftp.sh
+++ init/30_setup_lftp.sh
@@ -3,7 +3,7 @@
 cat >/script/lftp-sync-service.sh <<EOL
 #!/bin/bash
 
-/config/lftp-sync.sh -s \"$(printenv REMOTEDIR)\" -t \"/target\"
+/config/lftp-sync.sh -s "$(printenv REMOTEDIR)" -t \"/target\"
 EOL
 echo "Wrote /script/lftp-sync-service.sh"
 
